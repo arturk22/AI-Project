fe2476f98c38dcbfef3396b61b36da49c821eb08
Artur <alencar.artur00@gmail.com>
2023-09-18 19:04:31 -0300

    trilha 1

--- src/index.js
+++ src/index.js
@@ -49,6 +49,39 @@ async function processWhatsAppMessage(message) {
         message.from,
         "Obrigado por fornecer seus detalhes. Pode tirar suas duvidas..."
       );
+    } else if (!usersData[message.from].problemStage) {
+      client.sendText(
+        message.from,
+        "Por favor, escolha uma das opções numeradas abaixo para começar:\n" +
+        "1. Meu computador não liga.\n" +
+        "2. Meu computador está lento.\n" +
+        "3. Não consigo me conectar à internet.\n" +
+        "4. Tenho problemas com vírus ou malware.\n" +
+        "5. O som no meu computador não está funcionando.\n" +
+        "6. Meu monitor está sem sinal.\n" +
+        "7. Outro problema não listado.\n" +
+        "Por favor, selecione a opção que melhor descreve o seu problema ou escolha a opção 7 se estiver enfrentando um problema diferente. Estou aqui para ajudar!"
+      );
+      usersData[message.from].problemStage = "awaiting_problem_choice";
+      saveUsersData(usersData);
+    } else if (usersData[message.from].problemStage === "awaiting_problem_choice") {
+      const choice = parseInt(message.body);
+      if (choice >= 1 && choice <= 7) {
+        usersData[message.from].problemType = choice;
+        usersData[message.from].problemStage = "awaiting_problem_details";
+        saveUsersData(usersData);
+        client.sendText(message.from, "Por favor, forneça mais detalhes sobre o seu problema.");
+      } else {
+        client.sendText(message.from, "Escolha inválida. Por favor, selecione uma opção entre 1 e 7.");
+      }
+    } else if (usersData[message.from].problemStage === "awaiting_problem_details") {
+      const detailedDescription = message.body;
+      const choice = usersData[message.from].problemType;
+      const analise = await gerarAnalise(choice, detailedDescription, usersData[message.from].name);
+      client.sendText(message.from, analise);
+      delete usersData[message.from].problemStage;
+      delete usersData[message.from].problemType;
+      saveUsersData(usersData);
     } else {
       const analise = await gerarAnalise(
         message.body,
@@ -66,8 +99,9 @@ async function processWhatsAppMessage(message) {
   }
 }
 
-async function gerarAnalise(textoAnalise, userName) {
-  const promptText = `Dê possivei soluções para o problema do usuario: "${textoAnalise}"`;
+async function gerarAnalise(choice, detailedDescription, userName) {
+  const problemTypeDescription = gerarPrompt(choice);
+  const promptText = `${problemTypeDescription}. O usuário relatou: "${detailedDescription}". Forneça possíveis soluções.`;
 
   try {
     const completion = await openai.chat.completions.create({
@@ -81,6 +115,27 @@ async function gerarAnalise(textoAnalise, userName) {
   } catch (error) {
     console.error(error);
     throw error;
+  }
+}
+
+function gerarPrompt(choice) {
+  switch (choice) {
+    case 1:
+      return "Dê soluções para um computador que não liga.";
+    case 2:
+      return "Dê soluções para um computador que está lento.";
+    case 3:
+      return "Dê soluções para alguém que não consegue se conectar à internet.";
+    case 4:
+      return "Dê soluções para alguém com problemas de vírus ou malware.";
+    case 5:
+      return "Dê soluções para um computador cujo som não está funcionando.";
+    case 6:
+      return "Dê soluções para um monitor que está sem sinal.";
+    case 7:
+      return "Dê soluções gerais para problemas de computador não listados anteriormente.";
+    default:
+      return "Dê soluções gerais para problemas de computador.";
   }
 }
 
